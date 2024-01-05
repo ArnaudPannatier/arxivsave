63b35140167ade5b720d23f05817ebfc3ceb6a67
Arnaud Pannatier <arnaud.pannatier@idiap.ch>
2024-01-05 08:34:35 +0100

    Printing

--- arxiv/cli.py
+++ arxiv/cli.py
@@ -9,24 +9,28 @@ import requests
 def get_et(xml):
     it = ET.iterparse(StringIO(xml))
     for _, el in it:
-        _, has_namespace, postfix = el.tag.rpartition('}')
+        _, has_namespace, postfix = el.tag.rpartition("}")
         if has_namespace:
             el.tag = postfix  # strip all namespaces
     return it.root
+
 
 def query_article(id):
     params = {"id_list": id}
     res = requests.get("https://export.arxiv.org/api/query", params=params)
     return res.text
 
+
 def url_pdf(id):
     return "https://arxiv.org/pdf/{}.pdf".format(id)
 
-def save_pdf(title,id):
+
+def save_pdf(title, id):
     res = requests.get(url_pdf(id))
     path = Path.home() / "Documents" / "arxiv" / f"{title}.pdf"
     with path.open("ab") as f:
         f.write(res.content)
+
 
 def save(id=None):
     if id is None:
@@ -38,6 +42,6 @@ def save(id=None):
     tree = get_et(query_article(id))
     title = tree.find("entry").find("title").text
 
-    save_pdf(title.replace(" ", "-").replace("\n","").replace("--", "-").lower(),id)
+    print(f"Saving {title}...")
 
-
+    save_pdf(title.replace(" ", "-").replace("\n", "").replace("--", "-").lower(), id)
